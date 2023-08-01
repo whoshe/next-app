5a31815d249abd492104a7bc84e4b44d03d8624e
Changwoo Nam <whoshe@funnyplan.com>
2023-08-01 14:30:08 +0900

    검색 제안 기능 추가 사소한 변경

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
         <nav className="container navbar mx-auto">
           <a className="btn btn-lg btn-ghost text-lg">맞나? </a>
         </nav>
-        <section className="container mx-auto max-w-2xl flex flex-col justify-center flex-grow p-4">
+        <section className="container mx-auto max-w-3xl flex flex-col justify-center flex-grow p-4">
           <h1 className="text-3xl">
             어서오세요,
             <p className="py-2">
@@ -37,37 +37,50 @@ export default function Home() {
               을 만나세요.
             </p>
           </h1>
-          <search className="join w-full my-2 mb-6">
+
+          <search className="join w-full border rounded-full shadow-sm my-2">
             <button className="btn btn-lg rounded-l-full bg-transparent noHover join-item">
               {/** 동네서점 심볼 */}
               <MagnifyingGlassIcon className="h-6 w-6 absolute" />
             </button>
 
             <input value={value} onChange={onChange}
-              className="input input-lg text-2xl font-bold input-bordered rounded-l-full w-full join-item "
+              className="input input-lg text-2xl input-bordered rounded-l-full w-full join-item "
               placeholder="상호 또는 주소 입력.."
             />
             <button onClick={()=>onSearch(value)} className="btn btn-lg btn-primary rounded-r-full join-item">
               검색
             </button>
           </search>
-          <div className='mb-32'>
-            <ul className='menu menu-lg rounded-box border'>
+          
+          <search-result>
+            <ul className='w-full menu menu-lg rounded-box border shadow-sm bg-base-100'>
           {data.filter(item => {
             const searchTerm = value.toLowerCase();
             const fruitName = item.fruit.toLowerCase();
 
-            return searchTerm && fruitName.includes(searchTerm) && fruitName !== searchTerm;
+            return searchTerm && fruitName.startsWith(searchTerm) && fruitName !== searchTerm;
           }).slice(0,10)
           .map((item) => (
-            <li key={item.fruit} onClick={() => onSearch(item.fruit)} className=''><a><div>{item.fruit}
-            <span className='badge badge-md ml-2'>강원도 고성군</span></div></a></li>
+            <li key={item.fruit} onClick={() => onSearch(item.fruit)}>
+              <a>
+                <div clasName=''>
+                <MagnifyingGlassIcon className="h-6 w-6 text-gray-300 mr-1" /> 
+                </div>
+                <div>
+                  <div className='text-xl'>{item.fruit}</div>
+                  <div className='text-sm text-gray-400'>강원도 고성군 한누리대로 2135</div>
+                </div>
+            </a>
+            
+            </li>
           ))} 
             </ul>
-          </div>
-
+          </search-result>
 
         </section>
+
+
 
         <footer className="footer container mx-auto p-4">
           <div className="items-center grid-flow-col">
